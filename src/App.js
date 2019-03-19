447a6bc8441e8afa2cb3bdf11e7cf1b2bdb65cc5
KRMeserve <krmeserve@gmail.com>
2019-03-19 15:42:56 -0400

    work completed in technical interview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Button } from './components';
+import { ListCard } from './components';
 import './App.scss';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: ["Item 1", "Item 2", "Item 3"]
+    }
+  }
   render() {
     return (
       <div className="App">
         <header>
           <h1>Components</h1>
+          <ListCard header="Header" items={this.state.items}></ListCard>
         </header>
         <div className="component-list">
           <Button href="#" theme='go'> Button </Button>
--- src/components/list-card/ListCard.js
+++ src/components/list-card/ListCard.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { Card as Base } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-class ListCard extends Component {
+import './ListCard.scss';
 
+class ListCard extends Component {
+  constructor(props){
+    super(props);
+    this.mapArray = this.mapArray.bind(this);
+  }
+  mapArray(arr){
+    // let string = '';
+    // for (let i = 0; i < arr.length; i++) {
+    //   string += '- ' + arr[i];
+    // }
+    // return string;
+    return arr.map((string, index)=>{return <div class='item'>-  {arr[index]} </div>});
+  }
   render() {
     return (
       <Base>
-        Content...
+        <div className="header">{this.props.header}</div>
+        <div className="items">{this.mapArray(this.props.items)}</div>
       </Base>
     );
   }
